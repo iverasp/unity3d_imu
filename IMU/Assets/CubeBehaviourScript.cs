633be0ba78cabefed3e17bc12e0c23f4b4b055f4
Iver Egge <iverasp@gmail.com>
2016-08-29 18:50:37 +0200

    initial commit

---
+++ IMU/Assets/CubeBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject cube = gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A)) {
+			TransformCube(new Vector3(1, 0, 0));
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			TransformCube(new Vector3(0, 1, 0));
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			TransformCube(new Vector3(0, 0, 1));
+		}
+	}
+
+	void TransformCube(Vector3 eulerAngles) {
+		gameObject.transform.Rotate(eulerAngles);
+	}
+}
