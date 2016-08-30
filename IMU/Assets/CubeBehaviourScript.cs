10fe64104ace9437b38fe668d2e7675b2c599287
Iver Egge <iverasp@gmail.com>
2016-08-30 14:53:02 +0200

    added ble lib

--- IMU/Assets/CubeBehaviourScript.cs
+++ IMU/Assets/CubeBehaviourScript.cs
@@ -10,6 +10,15 @@ public class CubeBehaviourScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKey(KeyCode.Q)) {
+			TransformCube(new Vector3(-1, 0, 0));
+		}
+		if (Input.GetKey(KeyCode.W)) {
+			TransformCube(new Vector3(0, -1, 0));
+		}
+		if (Input.GetKey(KeyCode.E)) {
+			TransformCube(new Vector3(0, 0, -1));
+		}
 		if (Input.GetKey(KeyCode.A)) {
 			TransformCube(new Vector3(1, 0, 0));
 		}
@@ -19,6 +28,12 @@ public class CubeBehaviourScript : MonoBehaviour {
 		if (Input.GetKey(KeyCode.D)) {
 			TransformCube(new Vector3(0, 0, 1));
 		}
+
+		TransformCube (new Vector3 (Input.acceleration.x, 0, Input.acceleration.z));
+	}
+
+	void ReadFromSerial() {
+		
 	}
 
 	void TransformCube(Vector3 eulerAngles) {
